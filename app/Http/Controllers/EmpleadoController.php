096c8e42e234347b7db5f8144fc614d791f18148
yerkoz <yerkoz@gmail.com>
2021-09-07 21:05:11 -0300

    Add files via upload

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('empleado.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('empleado.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Empleado $empleado)
+    {
+        //
+    }
+}
